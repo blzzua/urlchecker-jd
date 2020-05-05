f71ea9216f8dafdc359ee8f51363c3ee7badb612
blzzua <blzzua@github.com>
2020-05-05 21:47:27 +0300

    handler on network(curl not return http status) errors

--- check.sh
+++ check.sh
@@ -2,9 +2,13 @@
 
 check_url_http_status (){
   URL="$1"
-  STATUSCODE=-1  # default.
+  DEFAULT_STATUSCODE=0  # default.
   STATUSCODE=$(curl -s -I "$URL" | grep -E -o  "HTTP/.{3} [0-9]{3}"  | cut -d\  -f2)
-  echo ${STATUSCODE}
+  if [[ "X$STATUSCODE" == "X" ]];
+  then
+    STATUSCODE="$DEFAULT_STATUSCODE"
+  fi
+  echo "$STATUSCODE"
 }
 
 
@@ -20,9 +24,9 @@ do
   STATUSCODE=$(check_url_http_status "$URL")
   ## - при любом "плохом" ответе от сервиса прерывать дальнейшую проверку.
   ## - "живой" - значит код ответа не 5XX или 4XX
-  if [[ "$STATUSCODE" -gt 400  ]] ;
+  if [[ "$STATUSCODE" -gt 400  ]] || [[  "$STATUSCODE" -lt 100  ]] ;
   then
-    echo "Выход при плохом коде ответа $STATUSCODE проверки  $URL"
+    echo -- "Выход при плохом коде ответа $STATUSCODE проверки  $URL"
     exit 1
   fi
   true

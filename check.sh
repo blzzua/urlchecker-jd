f7bd32e531b4695f57d60edc5259412715241439
blzzua <blzzua@github.com>
2020-05-05 22:49:32 +0300

    redir errors to stderr

--- check.sh
+++ check.sh
@@ -5,7 +5,8 @@ CURL_TIMEOUT=3 # sec
 check_url_http_status (){
   URL="$1"
   DEFAULT_STATUSCODE=0  # default.
-  STATUSCODE=$(curl --connect-timeout=${CURL_TIMEOUT} -s -I "$URL" | grep -E -o  "HTTP/.{3} [0-9]{3}"  | cut -d\  -f2)
+  #STATUSCODE=$(curl --connect-timeout=${CURL_TIMEOUT} -s -I "$URL" | grep -E -o  "HTTP/.{3} [0-9]{3}"  | cut -d\  -f2)
+  STATUSCODE=$(curl -s -o /dev/null -w "%{http_code}" "$URL")
   if [[ "X$STATUSCODE" == "X" ]];
   then
     STATUSCODE="$DEFAULT_STATUSCODE"
@@ -17,7 +18,7 @@ check_url_http_status (){
 URL_LIST=urls.txt
 if [[ ! -f "$URL_LIST" ]];
 then
-  echo "File $URL_LIST not found"
+  echo "File $URL_LIST not found" >&2
   exit 90
 fi
 
@@ -28,7 +29,7 @@ do
   ## - "живой" - значит код ответа не 5XX или 4XX
   if [[ "$STATUSCODE" -gt 400  ]] || [[  "$STATUSCODE" -lt 100  ]] ;
   then
-    echo -- "Выход при плохом коде ответа $STATUSCODE проверки  $URL"
+    echo -- "Выход при плохом коде ответа $STATUSCODE проверки  $URL" >&2
     exit 1
   fi
   true

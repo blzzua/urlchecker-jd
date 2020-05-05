2542c19458d9796765e30a05f3c5fedc5fd5e489
blzzua <blzzua@github.com>
2020-05-05 21:49:13 +0300

    add curl timeout

--- check.sh
+++ check.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+CURL_TIMEOUT=3 # sec
+
 check_url_http_status (){
   URL="$1"
   DEFAULT_STATUSCODE=0  # default.
-  STATUSCODE=$(curl -s -I "$URL" | grep -E -o  "HTTP/.{3} [0-9]{3}"  | cut -d\  -f2)
+  STATUSCODE=$(curl --connect-timeout=${CURL_TIMEOUT} -s -I "$URL" | grep -E -o  "HTTP/.{3} [0-9]{3}"  | cut -d\  -f2)
   if [[ "X$STATUSCODE" == "X" ]];
   then
     STATUSCODE="$DEFAULT_STATUSCODE"
